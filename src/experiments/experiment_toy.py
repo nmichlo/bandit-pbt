4aad5ed9b412093b9a6222b7f31269207ca83058
Nathan Michlo <NathanJMichlo@gmail.com>
2019-09-22 01:10:45 +0200

    better experiment running

--- src/experiments/experiment_toy.py
+++ src/experiments/experiment_toy.py
@@ -24,257 +24,97 @@ import dotenv
 # LOAD ENV
 dotenv.load_dotenv(dotenv.find_dotenv(), verbose=True)
 
-
-import os
-import pickle
-
 import comet_ml
-import scipy.stats
-from tqdm import tqdm
-from helper import util
-from uuid import uuid4
-
-from pbt.examples.pbt_paper_toy_example import ToyMember, ToyHyperParams
-from pbt.pbt import Population
-from pbt.strategies import ExploitUcb, ExploitTruncationSelection
 import numpy as np
+import matplotlib.pyplot as plt
 
-
-# ========================================================================= #
-# SYSTEM                                                                    #
-# ========================================================================= #
-
-
-class ToyExperimentLogger(object):
-    def __init__(self, experiment, is_ucb=True):
-        self.is_ucb = is_ucb
-        # COMET.ML
-        self._exp = experiment
-        # TOY-EXAMPLE: the type of member
-        # EXPLOIT-UCB: exploitation strategy
-        # PBT: pbt or ray-pbt
-        self._exp.add_tags(['toy-example', 'exploit-ucb' if self.is_ucb else 'exploit-truncation', 'pbt'])
-        self._exp.set_step(0)
-
-    def log_options(self, options, print_=True):
-        params = {
-            # EXPERIMENT
-            'steps': options['steps'],
-            'n': options['n'],
-            'repeats': options['repeats'],
-            'steps_till_ready': options['steps_till_ready'],
-            # UCB
-            **({
-                   'ucb_select_mode': options['select_mode'],
-                   'ucb_reset_mode': options['reset_mode'],
-                   'ucb_incr_mode': options['incr_mode'],
-                   'ucb_subset_mode': options['subset_mode'],
-                   'ucb_normalise_mode': options['normalise_mode'],
-                    **({'ucb_c': options['c']} if 'c' in options else {}),
-               } if self.is_ucb else {})
-        }
-        if print_:
-            print('\n#', '=' * 100, "#")
-            print(params)
-            print('#', '=' * 100, "#\n")
-        self._exp.log_parameters(params)
-        return params
-
-    def log_averages(self, name, values, values_target=None):
-        values = np.array(values)
-        self._exp.log_metrics({
-            f'ave_{name}': np.average(values),
-            f'ave_{name}_confidence': util.confidence_interval(values),
-        })
-        if values_target is not None:
-            values_target = np.array(values_target)
-            s_t, s_p = np.nan_to_num(scipy.stats.ttest_ind(values, values_target, equal_var=False))
-            self._exp.log_metrics({
-                f'ave_{name}_t_value': s_t,
-                f'ave_{name}_p_value': s_p,
-            })
-
-    def end(self):
-        # FINISH THE EXPERIMENT
-        self._exp.end()
-        self._exp = None
-
+from tsucb.helper.args import UcbExperimentArgs
+from tsucb.helper.util import print_separator
+from tqdm import tqdm
+from tsucb.helper import util
+from tsucb.pbt.examples.pbt_paper_toy_example import ToyMember, ToyHyperParams
+from tsucb.pbt.pbt import Population
 
 # ========================================================================== #
-# RUN EXPERIMENTS                                                            #
+# RUN EXPERIMENT                                                             #
 # ========================================================================== #
 
 
-def run_experiment(make_exploiter, options=None, show=True, test_scores=None, test_converges=None, test_seq=None):
-    is_ucb = isinstance(make_exploiter(), ExploitUcb)
+def run_experiment(args: UcbExperimentArgs):
+    EXP = comet_ml.Experiment(
+        disabled=args.disable_comet,
+        display_summary=True,
+    )
 
-    EXP = comet_ml.Experiment()
-    EXP_HELPER = ToyExperimentLogger(EXP, is_ucb=is_ucb)
+    EXP.add_tags([
+        'toy-example',
+        f'exploit-{args.pbt_exploiter}',
+        'pbt'
+    ])
 
     # OPTIONS
-    options = {**{
-        'steps': 50, 'n': 10, 'steps_till_ready': 2, 'exploration_scale': 0.1,
-        'exploit': True, 'explore': True, 'repeats': 100,
-    }, **(options or {})}
-    EXP_HELPER.log_options(options)
-    print('OPTIONS:', options)
+    print_separator(args.to_dict())
+    EXP.log_parameters(args.to_dict())
+
+    @util.min_time_elapsed(1.0)
+    def log_step(score, converge_time):
+        print('logged')
+        EXP.log_metrics({
+            f'max_score': score,
+            f'converge_time': converge_time,
+        })
 
     # LOOP VARS
-    scores, converges, score_seq = [], [], np.zeros(options['steps'])
-    itr = (tqdm(range(options['repeats'])) if show else range(options['repeats']))
+    scores, converge_times, avg_scores_per_step = [], [], np.zeros(args.pbt_steps)
 
     # LOOP
-    for i in itr:
+    for i in tqdm(range(args.experiment_repeats), disable=None):
         EXP.set_step(i+1)
 
-        # Members
+        # MEMBERS
         population = Population([
-            *[ToyMember(h=ToyHyperParams(np.random.rand(2) * 0.5, 0.01), theta=np.array([.9, .9])) for i in range(options['n'])],
-        ], exploiter=make_exploiter(), options=options)
-        # Train
-        population.train(options['steps'], exploit=options['exploit'], explore=options['explore'], show_progress=False)
+            *[ToyMember(h=ToyHyperParams(np.random.rand(2) * 0.5, 0.01), theta=np.array([.9, .9])) for i in range(args.pbt_members)],
+        ], exploiter=args.make_exploiter(), options={})
+        # TRAIN
+        population.train(args.pbt_steps, exploit=args.pbt_do_exploit, explore=args.pbt_do_explore, show_progress=False)
 
         # Calculates the score as the index of the first occurrence greater than 1.18
         _firsts = np.argmax(population.scores_history > 1.18, axis=1)
-        _firsts[_firsts == 0] = options['steps']
-        scores.append(np.max(population.scores))
-        converges.append(np.min(_firsts))
-        score_seq += population.scores_history.max(axis=0) * (1 / options['steps'])
+        _firsts[_firsts == 0] = args.pbt_steps
+        # STEP SCORES
+        score = np.max(population.scores)
+        converge_time = np.min(_firsts)
+        # AVERAGE SCORES
+        scores.append(score)
+        converge_times.append(converge_time)
+        avg_scores_per_step += population.scores_history.max(axis=0) * (1 / args.pbt_steps)
 
-        # The t score is a ratio between the difference between two groups and the difference within the groups.
-        # The larger the t score, the more difference there is between groups
-        # A p-value is the probability that the results from your sample data occurred by chance
-        # p < 0.05 is normally accepted as valid results
-        if test_scores is not None and test_converges is not None:
-            s, c, test_scores, test_converges = np.array(scores), np.array(converges), np.array(test_scores), np.array(test_converges)
-            s_t, s_p = scipy.stats.ttest_ind(s, test_scores, equal_var=False)
-            c_t, c_p = scipy.stats.ttest_ind(c, test_converges, equal_var=False)
-            if show:
-                itr.set_description(f's={s.mean().round(6)} (±{util.confidence_interval(s).round(6)}) [t,p]={np.around([s_t, s_p], 4)} | c={c.mean().round(4)} (±{util.confidence_interval(c).round(4)}) [t,p]={np.around([c_t, c_p], 4)}')
-        else:
-            if show:
-                s, c = np.array(scores), np.array(converges)
-                itr.set_description(f's={s.mean().round(6)} (±{util.confidence_interval(s).round(6)}) | c={c.mean().round(4)} (±{util.confidence_interval(c).round(4)})')
+        # LOG STEP
+        log_step(score, converge_time)
 
     # SCORES:       Maximum score acheived by each population
     # CONVERGES:    Minimum number of steps to converge for each population
     # SCORE_SEQ:    Average score at each time step
-    scores, converges, score_seq = np.array(scores), np.array(converges), np.array(score_seq)
+    scores, converge_times, avg_scores_per_step = np.array(scores), np.array(converge_times), np.array(avg_scores_per_step)
 
     # LOG
-    EXP_HELPER.log_averages('max_score', scores, test_scores)
-    EXP_HELPER.log_averages('converge_time', converges, test_converges)
+    EXP.log_metrics({
+        'ave_max_score': np.average(scores),
+        'ave_max_score_confidence_95': util.confidence_interval(scores, confidence=0.95),
+        'ave_converge_time': np.average(converge_times),
+        'ave_converge_time_confidence_95': util.confidence_interval(converge_times, confidence=0.95),
+    })
 
-    return scores, converges, score_seq
+    # LOG - PLOT
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(avg_scores_per_step)
+    EXP.log_figure(f'Average Score Per Step (repeats={args.experiment_repeats})', fig)
 
-
-def run_tests():
-    grid_search_options = dict(
-        c=[0.1, 0.05, 0.2],
-        select_mode=['ucb'], #, 'ucb_sample'],  #, 'uniform'}, # UCB SAMPLE IS USELESS
-        reset_mode=['exploited'], #, 'explored_or_exploited', 'explored'], # ALL OF THESE ARE THE SAME
-        subset_mode=['all', 'top', 'exclude_bottom'],
-        normalise_mode=['population', 'subset'],
-        incr_mode=['exploited', 'stepped'],
-        # test options
-        steps=20,
-        n=20,
-        repeats=2500,
-        steps_till_ready=2,
-    )
-
-    # HELPER
-    test_log = []
-    def append_results(id, options, results):
-        test_log.append([id, options, results])
-
-    # GRID SEARCH OPTIONS
-    search_options = list(enumerate(util.grid_search_named(grid_search_options)))
-    print(f'[GRID SEARCH PERMUTATIONS]: {len(search_options)}')
-
-    # FLITER SEARCH OPTIONS
-    exclude_options = []
-    keys = ['select_mode', 'reset_mode', 'incr_mode', 'subset_mode', 'normalise_mode', 'c']
-    exclude_options = {tuple(s[k] for k in keys) for s in exclude_options}
-    search_options = [(i, s) for i, s in search_options if tuple(s[k] for k in keys) not in exclude_options]
-    print(f'[GRID SEARCH REMAINING]: {len(search_options)}')
-
-    # RUN BASELINE EXPERIMENT
-    results = run_experiment(lambda: ExploitTruncationSelection(), grid_search_options, show=True)
-    append_results(0, grid_search_options, results)
-
-    # RUN EXPERIMENTS
-    for i, options in search_options:
-        def make_exploiter():
-            return ExploitUcb(**{k: options[k] for k in ['subset_mode', 'incr_mode', 'reset_mode', 'select_mode', 'normalise_mode', 'c']})
-        r = run_experiment(make_exploiter, options, show=True) #, test_scores=results[0], test_converges=results[1])
-        append_results(0, options, r)
-
-    print('\nDONE!\n')
-    save_path = f'../../../results/results_toy_{uuid4()}.pickle'
-    print(f'SAVING RESULTS TO: {save_path}')
-    with open(save_path, 'wb') as file:
-        pickle.dump(test_log, file)
+    # END EXPERIMENT
+    EXP.end()
 
 
 if __name__ == '__main__':
-    os.environ['COMET_PROJECT_NAME'] = 'improving-pbt-toy-examples-fixes-3'
-    run_tests()
+    args = UcbExperimentArgs.from_parsed_args()
+    run_experiment(args)
 
-
-# ========================================================================== #
-# UPLOAD OLD SAVED EXPERIMENTS                                               #
-# ========================================================================== #
-
-
-# def upload_experiment(
-#         id, options,
-#         scores, converges, score_seq,
-#         trunc_scores=None, trunc_converges=None, trunc_score_seq=None, is_ucb=True
-# ):
-#     assert len(scores) == len(converges)
-#     EXP = comet_ml.OfflineExperiment(
-#         # DISABLE
-#         disabled=False,
-#         # CODE, ENV, GIT
-#         log_code=False,
-#         log_env_details=False,
-#         log_git_metadata=False,
-#         log_git_patch=False,
-#         # HOST, GPU, CPU
-#         log_env_gpu=False,
-#         log_env_host=False,
-#         log_graph=False,
-#     )
-#     logger = ToyExperimentLogger(EXP, is_ucb=is_ucb)
-#     logger.log_options(options)
-#     logger.log_averages('max_score', scores, trunc_scores)
-#     logger.log_averages('converge_time', converges, trunc_converges)
-#     logger.end()
-#
-#
-# def upload_results_to_comet(test_log):
-#     # MAKE DIRS
-#     if not os.path.isdir(os.environ['COMET_OFFLINE_DIRECTORY']):
-#         print('[MAKING DIRS]', os.environ['COMET_OFFLINE_DIRECTORY'])
-#         os.makedirs(os.environ['COMET_OFFLINE_DIRECTORY'], exist_ok=True)
-#
-#     # LOAD TRUNCATION SELECTION DATA
-#     trunc_id, trunc_options, (trunc_scores, trunc_converges, trunc_score_seq) = test_log[0]
-#     upload_experiment(trunc_id, trunc_options, trunc_scores, trunc_converges, trunc_score_seq, is_ucb=False)
-#
-#     # LOAD UCB DATA
-#     for id, options, (scores, converges, score_seq) in tqdm(test_log[1:]):
-#         upload_experiment(id, options, scores, converges, score_seq, trunc_scores, trunc_converges, trunc_score_seq, is_ucb=True)
-
-
-# if __name__ == '__main__':
-#     with open('../../../results/results_toy-pbt_ucb-vs-trunv_grid-search_5000iters.pickle', 'rb') as file:
-#         test_log = pickle.load(file)
-#         upload_results_to_comet(test_log)
-
-
-
-# ========================================================================= \#
-# END                                                                       \#
-# ========================================================================= \#
---
+++ src/helper/args.py
@@ -0,0 +1,152 @@
+
+#  Copyright (c) 2019 Nathan Juraj Michlo
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy
+#  of this software and associated documentation files (the "Software"), to deal
+#  in the Software without restriction, including without limitation the rights
+#  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+#  copies of the Software, and to permit persons to whom the Software is
+#  furnished to do so, subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+#  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+#  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+#  SOFTWARE.
+
+
+import argparse
+from typing import NamedTuple
+from tsucb.pbt.pbt import IExploiter
+
+
+# ========================================================================= #
+# args                                                                      #
+# ========================================================================= #
+
+
+def argparse_number_range(a, b, number_type):
+    assert number_type in {int, float}
+    def inner(x):
+        x = number_type(x)
+        if not (a <= x <= b):
+            raise argparse.ArgumentTypeError("%r not in range [0.0, 1.0]"%(x,))
+        return x
+    return inner
+
+
+# ========================================================================= #
+# UCB EXPERIMENT                                                            #
+# ========================================================================= #
+
+
+class UcbExperimentArgs(NamedTuple):
+    # EXPERIMENT
+    experiment_repeats: int
+    # PBT
+    pbt_steps: int
+    pbt_members: int
+    pbt_members_ready_after: int
+    pbt_exploiter: str
+    pbt_do_exploit: bool
+    pbt_do_explore: bool
+    # EXPLOITER - UCB
+    ucb_select_mode: str
+    ucb_incr_mode: str
+    ucb_reset_mode: str
+    ucb_subset_mode: str
+    ucb_normalise_mode: str
+    ucb_c: float
+    # EXPLOITER - UCB & TS
+    ts_ratio_top: float
+    ts_ratio_bottom: float
+    # EXTRA
+    debug: bool
+    disable_comet: bool
+
+    @property
+    def is_ucb(self):
+        return self.pbt_exploiter == 'ucb'
+
+    def to_dict(self):
+        return {k: getattr(self, k) for k in self._fields}
+
+    def make_exploiter(self) -> 'IExploiter':
+        if self.pbt_exploiter == 'ucb':
+            from tsucb.pbt.strategies import ExploitUcb
+            return ExploitUcb(
+                bottom_ratio=self.ts_ratio_bottom,
+                top_ratio=self.ts_ratio_top,
+                c=self.ucb_c,
+                subset_mode=self.ucb_subset_mode,
+                incr_mode=self.ucb_incr_mode,
+                reset_mode=self.ucb_reset_mode,
+                select_mode=self.ucb_select_mode,
+                normalise_mode=self.ucb_normalise_mode,
+                debug=self.debug
+            )
+        elif self.pbt_exploiter == 'ts':
+            from tsucb.pbt.strategies import ExploitTruncationSelection
+            return ExploitTruncationSelection(
+                bottom_ratio=self.ts_ratio_bottom,
+                top_ratio=self.ts_ratio_top,
+            )
+        else:
+            raise KeyError(f'Invalid exploiter: {self.pbt_exploiter}')
+
+    def make_ray_exploiter(self):
+        raise NotImplementedError('This is not yet implemented')
+
+    @staticmethod
+    def _create_parser(defaults=None) -> 'argparse.ArgumentParser':
+        if defaults is None:
+            defaults = {}
+
+        parser = argparse.ArgumentParser()
+        # EXPERIMENT
+        parser.add_argument('--experiment-repeats',      type=argparse_number_range(1, float('inf'), int),                           default=defaults.get('experiment_repeats', 1))
+        # PBT
+        parser.add_argument('--pbt-steps',               type=argparse_number_range(1, float('inf'), int),                           default=defaults.get('pbt_steps', 20))
+        parser.add_argument('--pbt-members',             type=argparse_number_range(1, float('inf'), int),                           default=defaults.get('pbt_members', 10))
+        parser.add_argument('--pbt-members-ready-after', type=argparse_number_range(1, float('inf'), int),                           default=defaults.get('pbt_members_ready_after', 2))
+        parser.add_argument('--pbt-exploiter',           type=str.lower, choices=['ucb', 'ts'],                                      default=defaults.get('pbt_exploiter', 'ucb'))
+        parser.add_argument('--pbt-disable-exploit',     action='store_false', dest='pbt_do_exploit')
+        parser.add_argument('--pbt-disable-explore',     action='store_false', dest='pbt_do_explore')
+        # EXPLOITER - UCB
+        parser.add_argument('--ucb-select-mode',         type=str.lower, choices=['ucb', 'ucb_sample', 'uniform'],                   default=defaults.get('ucb_select_mode', 'ucb'))
+        parser.add_argument('--ucb-incr-mode',           type=str.lower, choices=['exploited', 'stepped'],                           default=defaults.get('ucb_incr_mode', 'exploited'))
+        parser.add_argument('--ucb-reset-mode',          type=str.lower, choices=['exploited', 'explored', 'explored_or_exploited'], default=defaults.get('ucb_reset_mode', 'exploited'))
+        parser.add_argument('--ucb-subset-mode',         type=str.lower, choices=['all', 'exclude_bottom', 'top'],                   default=defaults.get('ucb_subset_mode', 'all'))
+        parser.add_argument('--ucb-normalise-mode',      type=str.lower, choices=['population', 'subset'],                           default=defaults.get('ucb_normalise_mode', 'population'))
+        parser.add_argument('--ucb-c',                   type=argparse_number_range(0, 1, float),                                    default=defaults.get('ucb_c', 0.1))
+        # EXPLOITER - UCB & TS
+        parser.add_argument('--ts-ratio-top',            type=argparse_number_range(1, float('inf'), int),                           default=defaults.get('ts_ratio_top', 0.2))
+        parser.add_argument('--ts-ratio-bottom',         type=argparse_number_range(1, float('inf'), int),                           default=defaults.get('ts_ratio_bottom', 0.2))
+        # EXTRA
+        parser.add_argument('--debug',                   action='store_true')
+        parser.add_argument('--enable-comet',            action='store_false', dest='disable_comet')
+
+        return parser
+
+    @staticmethod
+    def _from_args(args: argparse.Namespace):
+        return UcbExperimentArgs._from_dict(vars(args))
+
+    @staticmethod
+    def _from_dict(args) -> 'UcbExperimentArgs':
+        return UcbExperimentArgs(*[args[field] for field in UcbExperimentArgs._fields])
+
+    @staticmethod
+    def from_parsed_args(defaults=None) -> 'UcbExperimentArgs':
+        parser = UcbExperimentArgs._create_parser(defaults=defaults)
+        return UcbExperimentArgs._from_args(parser.parse_args())
+
+
+# ========================================================================= #
+# END                                                                       #
+# ========================================================================= #
